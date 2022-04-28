ff85de738107960633aa5e3e57b82931ecc04c58
FzrFernando <fernandozamorarico@gmail.com>
2022-04-28 12:30:14 +0200

    Avances en elementos

---
+++ JuegoJaca/src/elementos/Coordenada.java
@@ -0,0 +1,27 @@
+package elementos;
+
+public class Coordenada {
+
+	private int x;
+	private int y;
+	
+	public Coordenada() {
+		super();
+	}
+
+	public Coordenada(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+	
+	
+}
